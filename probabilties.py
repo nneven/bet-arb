f3b01dde488f2c5e67de3e6e1d6ecde2c6aa17bd
Bebop <nneven@usc.edu>
2022-12-30 10:21:03 -0800

    Update probabilties.py

--- probabilties.py
+++ probabilties.py
@@ -4,6 +4,7 @@ Description:
 """
 
 # Imports
+from tabulate import tabulate
 from betonline import BetOnline
 from bovada import Bovada
 
@@ -15,18 +16,34 @@ from bovada import Bovada
 
 # Main
 def main():
-    
+
     print()
     print('Bet Online')
     betonline = BetOnline()
-    soccer_games = betonline.get_sport('soccer')
-    print(soccer_games)
+    bet_online_soccer_games = betonline.get_sport('soccer')
+    print(bet_online_soccer_games)
 
     print()
     print('Bovada')
     bovada = Bovada()
-    soccer_games = bovada.get_sport('soccer')
-    print(soccer_games)
+    bovada_soccer_games = bovada.get_sport('soccer')
+    print(bovada_soccer_games)
+
+    print()
+    game_count = 0
+    for i in range(len(bet_online_soccer_games)):
+        for j in range(len(bovada_soccer_games)):
+            if bet_online_soccer_games['Team 1'][i][:4] == bovada_soccer_games['Team 1'][j][:4] and \
+                bet_online_soccer_games['Team 2'][i][:4] == bovada_soccer_games['Team 2'][j][:4]:
+
+                game_count += 1
+                headers = ['', bet_online_soccer_games['Team 1'][i], bet_online_soccer_games['Team 2'][i], 'Draw']
+                table = [['Bet Online', bet_online_soccer_games['Odds 1'][i], bet_online_soccer_games['Odds 2'][i], bet_online_soccer_games['Draw'][i]],
+                        ['Bovada', bovada_soccer_games['Odds 1'][j], bovada_soccer_games['Odds 2'][j], bovada_soccer_games['Draw'][j]]]
+
+                print('Game', game_count, ':', bet_online_soccer_games['Team 1'][i], 'vs', bet_online_soccer_games['Team 2'][i])
+                print(tabulate(table, headers=headers, tablefmt='simple_grid', numalign='right', floatfmt='.2f'))
+                print()
 
 if __name__ == "__main__":
     main()
